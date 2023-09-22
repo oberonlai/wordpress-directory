fca9ca34995a01504853e6d3a2363e642845fff6
oberonlai <m615926@gmail.com>
2023-09-22 13:08:13 +0800

    add plugin interface

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,7 @@ export default function Command() {
     execute: searchText.length > 0,
 	parseResponse: parseFetchResponse,
   });
-
- console.log(data);
  
-  
 
   return (
     <List
@@ -41,21 +38,22 @@ export default function Command() {
 
 }
 
-async function parseFetchResponse(response) {
+async function parseFetchResponse(response: Response) {
   if (!response.ok) {
     throw new Error(response.statusText);
   }
   const responseData = await response.json();
   const plugins = responseData.plugins || [];
-  const parsedData = plugins.map((plugin) => ({
+  const parsedData = plugins.map((plugin: Plugin) => ({
     name: plugin.name.replace(/&#8211;/g, "-"),
-    description: plugin.description.replace(/<[^>]+>/g, '\n'),
+    description: plugin.description?.replace(/<[^>]+>/g, '\n') || plugin.short_description?.replace(/<[^>]+>/g, '\n'),
 	url: `https://wordpress.org/plugins/${plugin.slug}/`,
 	icon: plugin.icons['1x'],
 	download: plugin.download_link,
 	version: plugin.version,
 	rating: Math.ceil((plugin.ratings['1']/plugin.num_ratings)*100),
 	solved: Math.ceil((plugin.support_threads_resolved/plugin.support_threads)*100),
+	active: plugin.active_installs.toLocaleString(),
 	downloaded: plugin.downloaded.toLocaleString(),
 	update:plugin.last_updated
   } as SearchResult));
@@ -96,7 +94,7 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
             <DefaultActions searchResult={searchResult} />
             {environment.canAccess(AI) && (
               <Action.Push
-                title="View Summary"
+                title="Translate Plugin Description in Tradtional Chinese"
                 icon={Icon.Paragraph}
                 shortcut={{ modifiers: ["cmd", "shift"], key: "enter" }}
                 target={<Summary searchResult={searchResult} />}
@@ -110,16 +108,16 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
 		  markdown={searchResult?.description}
 		  metadata={
 			<List.Item.Detail.Metadata>
-				<List.Item.Detail.Metadata.Link
-			    title="Download Zip"
-			    target={searchResult?.download || '#'}
-			    text="Download from WordPress.org"
-			  />
-				<List.Item.Detail.Metadata.Label title="Version" text={searchResult?.version} />
+			  <List.Item.Detail.Metadata.Label title="Downloaded" text={searchResult?.downloaded || 'loading...'} />
+			  <List.Item.Detail.Metadata.Label title="Active Installs" text={searchResult?.active || 'loading...'} />
+			  <List.Item.Detail.Metadata.Label title="Last Updated" text={searchResult?.update || 'loading...'} />
 			  <List.Item.Detail.Metadata.Label title="1 Star ratio" text={searchResult?.rating+'%' || 'loading...'} />
 			  <List.Item.Detail.Metadata.Label title="Problem-solving ratio" text={searchResult?.solved+'%' || 'loading...'} />
-			  <List.Item.Detail.Metadata.Label title="Downloaded" text={searchResult?.downloaded || 'loading...'} />
-			  <List.Item.Detail.Metadata.Label title="Last Updated" text={searchResult?.update || 'loading...'} />
+			  <List.Item.Detail.Metadata.Link
+			    title="Download Zip"
+			    target={searchResult?.download || '#'}
+			    text={'version ' + searchResult?.version}
+			  />
 			</List.Item.Detail.Metadata>
 		  }
 		/>
@@ -138,14 +136,14 @@ function DefaultActions({ searchResult }: { searchResult: SearchResult }) {
 }
 
 function Summary({ searchResult }: { searchResult: SearchResult }) {
-  const item = JSON.stringify(searchResult);
-  const prompt = `Summarize the following from the WordPress documentation and give one example of usage in a code block. Add the language to the code block like \`\`\`php. The context can only be about WordPress. Format the response as if you are providing documentation:\n${item}`;
+  const item = JSON.stringify(searchResult.description.substring(0, 2000));
+  const prompt = `Translate the following from the WordPress plugin description in Tradtional Chinese. The context can only be about WordPress. Format the response as if you are providing documentation:\n${item.replace('\n','')}`;
   const { data, isLoading } = useAI(prompt, { creativity: 0 });
   const code = data.match(/```[\w\S]*\n([\s\S]*?)\n```/);
 
   return (
     <Detail
-      navigationTitle="AI Generated Summary"
+      navigationTitle="AI Translate Description"
       isLoading={isLoading}
       markdown={data}
       actions={
@@ -159,7 +157,7 @@ function Summary({ searchResult }: { searchResult: SearchResult }) {
             <DefaultActions searchResult={searchResult} />
             <Action.CopyToClipboard
               shortcut={{ modifiers: ["cmd", "shift"], key: "enter" }}
-              title="Copy Summary To Clipboard"
+              title="Copy Translation To Clipboard"
               content={data}
             />
             {code?.[1] ? (
@@ -188,4 +186,26 @@ interface SearchResult {
   solved: number;
   downloaded: string;
   update: string;
+  active: string;
 }
+
+interface Plugin {
+	name: string;
+	description: string;
+	short_description: string;
+	slug: string;
+	version: string;
+	icons: {
+		'1x': string;
+	};
+	ratings: {
+		'1': number;
+	};
+	download_link: string;
+	num_ratings: number;
+	support_threads_resolved: number;
+	support_threads: number;
+	active_installs: number;
+	downloaded: number;
+	last_updated: string;
+  }
